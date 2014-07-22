a3ce1297032ca4ff1d91bcbe5870e2c2436f57b8
Gitthere <ichi4g@gmail.com>
2014-07-22 13:04:21 -1000

    app.get, res render of list, edit new complete

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-mongodb://toodoo_express:toodoo_express@ds027709.mongolab.com:27709/toodoo_express;
-// override with POST having ?_method=DELETE app.use(methodOverride('_method'));
+mongoose.connect('mongodb://toodoo_express:toodoo_express@ds027709.mongolab.com:27709/toodoo_express');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var jade = require('jade');
 
 //must come AFTER app.use(bodyParser)
+app.set('views', __dirname + '/templates/');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var methodOverride = require('method-override');
 
-app.set('views', __dirname + '/templates');
+
+app.get('/tasks/', function (req,res){
+  console.log('test res render in app.get');
+  res.render('tasks/list.jade');
+  //res.send('hello world');
+  //res.send('tasks/list.jade');//worked with hello world, now not working.  
+  //how does tasks/list.jade work?  what is jade layout and what is relation to this?
+});
+
+app.get('/tasks/edit', function (req,res) {
+  res.render('tasks/edit.jade');
+});
+
+app.get('/tasks/new', function (req,res) {
+  res.render('tasks/new.jade');
+});
+
+// res.render('layout.jade', function() { doesn't work like this, needs to be used with app.get
+//   console.log('check res render function');
+// }); //is res.render to be used with app.get or app.set?
     
 
 
-app.get('/', function (req,res){
-  res.send('hello wonka');
-});
+
 
 
 //LIST
