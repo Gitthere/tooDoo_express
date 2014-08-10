59a695e3ec74f38a0fb3e9027c26b205de163bda
Gitthere <ichi4g@gmail.com>
2014-08-09 19:27:25 -1000

    created routes/index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-// router.use(function(req,res,next) {
-//   if(!req.user){
-//     res.redirect('/users/login');
-//   }
-// //   next();
-// });
+
 
 router.get('/',function(req, res) {
   res.send('Index Page');//do you want to sign in?
 });
 
+
+//LOGIN - renders login form
+router.get('/login', function (req,res) {
+  res.render('users/login.jade');
+});
+
+
+//LOGOUT
+router.get('/logout', function (req, res) {
+  req.session.user = undefined;
+  res.redirect('/login');
+
+});
+
 module.exports = router;
+
